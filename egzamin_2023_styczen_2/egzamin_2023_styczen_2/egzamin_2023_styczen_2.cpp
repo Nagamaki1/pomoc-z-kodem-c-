6ca2b341530ac6d8dfadef0a2a49f42c4b95f097
Nagamaki1 <134847554+Nagamaki1@users.noreply.github.com>
2023-05-31 22:11:12 +0200

    Add files via upload

---
+++ egzamin_2023_styczen_2/egzamin_2023_styczen_2/egzamin_2023_styczen_2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Notatka {
+private:
+	static int number ;
+	int Ide;
+public:
+	string tytul, tresc;
+
+	Notatka(string ty="tytul", string tr ="tresc") {
+		ty = tytul;
+		tr = tresc;
+		number++;
+		Ide = number;
+		
+
+		
+	}
+	void wpis() {
+		cout << "Wpisz tytul:" << endl;
+		cin >> tytul;
+		cout << "Wpisz tresc:" << endl;
+		cin >> tresc;
+	}
+	void wyswietl() {
+		cout << number << endl;
+		cout << Ide << endl;
+		cout << tytul << endl;
+		cout << tresc;
+	}
+	void diagnos() {
+
+	}
+	
+	
+};
+int Notatka::number = 0;
+int main()
+{
+	Notatka z1;
+	Notatka z2;
+	z1.wpis();
+	z1.wyswietl();
+	z2.wpis();
+	z2.wyswietl();
+	return 0;
+}
+
