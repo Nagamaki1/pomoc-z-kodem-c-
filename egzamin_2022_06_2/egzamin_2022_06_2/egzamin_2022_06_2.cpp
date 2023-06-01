fb266e4789dc6eb1073315fd98af1844179e6faa
Nagamaki1 <134847554+Nagamaki1@users.noreply.github.com>
2023-06-01 18:14:49 +0200

    Add files via upload

---
+++ egzamin_2022_06_2/egzamin_2022_06_2/egzamin_2022_06_2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Osoba {
+	private:
+		
+	int Id;
+	string imie;
+public:
+static int numer ;
+	
+	Osoba() {
+		numer = Id;
+		numer++;
+		cout << "Id:"<< Id <<endl;
+		cout << "Imie:"<< imie << endl;
+	}
+	Osoba(const Osoba&);
+	Osoba& operator=(const Osoba& Id);
+	
+	Osoba() {
+		numer++;
+	}
+	~Osoba() { numer--; }
+};
+int Osoba::numer = 0;
+int main()
+{
+	Osoba z1;
+
+
+
+
+}
